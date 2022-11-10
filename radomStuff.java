093e2e80e75b8b614b13f06258e6889971ad921d
UserUnknown06 <65127957+UserUnknown06@users.noreply.github.com>
2022-11-09 17:24:04 -0800

    Add files via upload
    复健。

---
+++ radomStuff.java
@@ -0,0 +1,32 @@
+//Since I'm a sb in the first place, I'm writing tones of comments, just in case I forgot.
+import java.util.Scanner;
+
+public class randomStuff{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("How many are enrolling? ");
+    int count = sc.nextInt();//input how many times it repeats.
+    System.out.println();//makes it looks good
+
+    String[] enrollingNames;
+    enrollingNames = new String[count];//define a string array
+    int[] enrollingAges;
+    enrollingAges = new int[count];//define a integer array;
+
+    for(int loop = 0; loop < count; loop++){
+      System.out.println("Input name: ");
+      enrollingNames[loop] = sc.next();//input name(great，至少可以input进去了！)
+      System.out.println("Input age: ");
+      enrollingAges[loop] = sc.nextInt();//input age
+
+      System.out.println(enrollingNames[loop]+", "+enrollingAges[loop]+", recorded. ");
+      System.out.println();
+    }//for
+
+    System.out.println("All inputs: ");
+
+    for(int loop = 0; loop < count; loop++){
+      System.out.println(enrollingNames[loop]+", "+enrollingAges[loop]);
+    }//for
+  }//main
+}//class
